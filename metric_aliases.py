495ad0676ffbd194fb7c1722d8185b8b2d38dbd7
Anvi Singh <anvi4242@gmail.com>
2026-07-06 23:46:24 +0530

    Add reusable SEC metric aliases

---
+++ metric_aliases.py
@@ -0,0 +1,89 @@
+"""Reusable US-GAAP tag aliases, ordered from preferred to fallback tags."""
+
+METRIC_ALIASES: dict[str, tuple[str, ...]] = {
+    "Revenue": (
+        "RevenueFromContractWithCustomerExcludingAssessedTax",
+        "RevenueFromContractWithCustomerIncludingAssessedTax",
+        "SalesRevenueNet",
+        "Revenues",
+    ),
+    "Net Income": (
+        "NetIncomeLoss",
+        "ProfitLoss",
+        "NetIncomeLossAvailableToCommonStockholdersBasic",
+    ),
+    "EBIT": (
+        "OperatingIncomeLoss",
+        "IncomeLossFromContinuingOperationsBeforeInterestExpenseInterestIncomeIncomeTaxesExtraordinaryItemsNoncontrollingInterestsNet",
+    ),
+    "EBT": (
+        "IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest",
+        "IncomeLossFromContinuingOperationsBeforeIncomeTaxesMinorityInterestAndIncomeLossFromEquityMethodInvestments",
+        "IncomeLossFromContinuingOperationsBeforeIncomeTaxes",
+    ),
+    "Interest Expense": (
+        "InterestExpenseNonoperating",
+        "InterestAndDebtExpense",
+        "InterestExpense",
+        "InterestExpenseNet",
+        "InterestIncomeExpenseNonoperatingNet",
+    ),
+    "Interest Income": (
+        "InterestIncome",
+        "InvestmentIncomeInterest",
+        "InterestAndDividendIncome",
+    ),
+    "Cash": (
+        "CashAndCashEquivalentsAtCarryingValue",
+        "CashCashEquivalentsRestrictedCashAndRestrictedCashEquivalents",
+        "Cash",
+    ),
+    "Investments": (
+        "ShortTermInvestments",
+        "MarketableSecuritiesCurrent",
+        "AvailableForSaleSecuritiesCurrent",
+        "DebtSecuritiesAvailableForSaleCurrent",
+    ),
+    "Debt": (
+        "LongTermDebtAndFinanceLeaseObligationsCurrent",
+        "LongTermDebtCurrent",
+        "ShortTermBorrowings",
+        "LongTermDebtNoncurrent",
+        "LongTermDebtAndFinanceLeaseObligationsNoncurrent",
+        "LongTermDebt",
+    ),
+    "Preferred Equity": (
+        "PreferredStockValue",
+        "PreferredStockCarryingValue",
+        "TemporaryEquityCarryingAmountAttributableToParent",
+        "PreferredStocksIncludingAdditionalPaidInCapital",
+    ),
+    "D&A": (
+        "DepreciationDepletionAndAmortization",
+        "DepreciationAndAmortization",
+        "Depreciation",
+        "AmortizationOfIntangibleAssets",
+    ),
+    "Impairment": (
+        "AssetImpairmentCharges",
+        "GoodwillImpairmentLoss",
+        "ImpairmentOfIntangibleAssetsExcludingGoodwill",
+        "RestructuringAndImpairmentCharges",
+        "PropertyPlantAndEquipmentImpairment",
+    ),
+}
+
+
+LTM_METRICS = (
+    "Revenue",
+    "Net Income",
+    "EBIT",
+    "EBT",
+    "Interest Expense",
+    "Interest Income",
+    "D&A",
+    "Impairment",
+)
+
+BALANCE_SHEET_METRICS = ("Cash", "Investments", "Debt", "Preferred Equity")
+
